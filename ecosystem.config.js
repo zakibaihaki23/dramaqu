6d371361d101a6628eaa5e867c0592fd920c9854
Tebe <zakibaihaki84@gmail.com>
2025-12-29 11:57:20 +0700

    feat: add ecosystem configuration for dramaqu-bot

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [
+    {
+      name: "dramaqu-bot",
+      script: "server/telegram-bot.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+      // Load environment variables from .env file
+      env_file: ".env",
+    },
+  ],
+}
